33423378b8d35ba29011ffa4987d5d4bd36f9cf7
Ethan Coles <ethanjcoles@gmail.com>
2024-04-22 17:05:08 -0700

    added some comments

--- src/components/TranslationStation.js
+++ src/components/TranslationStation.js
@@ -1,3 +1,5 @@
+//not technically a component so sorry for putting this in the components folder
+//this is a placeholder, im currently working on getting access to the cloud translate api
 
 function translate(input, lang) {
     if (lang === 'English') {
