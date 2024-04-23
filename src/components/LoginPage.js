4be569393b3abea3f0a2eeb305b4d73674b77dd6
Sulaiman Waziri <SulamiWizard@gmail.com>
2024-04-23 12:58:07 -0700

    Implemented The Login Page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,25 @@
-import {Link} from 'react-router-dom';
-import translate from "./TranslationStation";
+import { Link } from 'react-router-dom';
+import './LoginPage.css'; // Import CSS file for styling
+import logo from '../assets/hornetGuy.png'; //website logo
 
-function LoginPage(props) {
+function LoginPage() {
+    return (
+        <div className="login-page">
+            <div className="background-image"></div>
 
-    const language = props.language[0]
-
-    return(
-        <>
-            <p>{translate("this is the login page, work in progress", language)}</p>
-            <Link to='/'>
-                <button>
-                    {translate("GO BACK", language)}
-                </button>
-            </Link>
-        </>
+            <div className="login-container">
+                <img src={logo} alt="Website Logo" className="logo-image" /> {/* Circular Logo */}
+                <p style={{ marginBottom: '20px' }}>Don’t have an account? <Link to="/register">Click here to register for one!</Link></p> {/* Text */}
+                <form>
+                    {/* Username input field */}
+                    <input className="username-input" type="text" placeholder="Username" />
+                    {/* Password input field */}
+                    <input className="password-input" type="password" placeholder="Password" />
+                    {/* Login button */}
+                    <button className="login-button" type="submit">Login</button>
+                </form>
+            </div>
+        </div>
     );
 }
 
