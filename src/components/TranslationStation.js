c4e68b8ebd159c84981366c84cf42c465f2c6a9c
Sulaiman Waziri <SulamiWizard@gmail.com>
2024-05-03 22:08:58 -0700

    Slightly working Translation for UI navbar

--- src/components/TranslationStation.js
+++ src/components/TranslationStation.js
@@ -1,52 +1,30 @@
-//not technically a component so sorry for putting this in the components folder
-//this is a placeholder, im currently working on getting access to the cloud translate api
-import { useState } from "react";
-import ListOfLanguages from "./ListOfLanguages";
 import axios from "axios";
+import ListOfLanguages from "./ListOfLanguages";
 
+const baseUrl = "http://localhost:8080/api/translation";
 
-
-const baseUrl = "http://localhost:8080/api/translation"
-
- function translate(input, lang) {
-
-    const [isLoaded, setIsLoaded] = useState([false, "loading"]);
-
+async function translate(input, lang) {
     if (lang === 'English') {
-        return(input)
-
-    } else if(!isLoaded[0]) {
-        const target = getLanguageCode(lang)
-
-        axios.get(baseUrl + "/" + target + "/" + input).then( response => {
-            setIsLoaded([true, response.data])
-        })
-
-        return("loading");
-
-    } else if (isLoaded[0]) {
-        return(isLoaded[1])
-    }
-}
-
-
-function getLanguageCode(lang) {
-    for (var i = 0; i < ListOfLanguages.length; i++) {
-        if (ListOfLanguages[i][1] === lang) {
-            return (ListOfLanguages[i][0]);
+        return input;
+    } else {
+        const target = getLanguageCode(lang);
+        try {
+            const response = await axios.get(`${baseUrl}/${target}/${input}`);
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching translation:", error);
+            return `Translation error: ${error.message}`; // Include error message in the return value
         }
     }
-//     const [isSending, setIsSending] = useState(false)
-//     const tagsClick = useCallback(async (id) => {
-     
-//     if (isSending) return
-//     setIsSending(true)
-//     await getAllPostsByTagId([id]).then(res => {
-//         handleDataFromChild(res.data);
-//     });
-//     setIsSending(false)
-//   }, [isSending])
 }
 
+function getLanguageCode(lang) {
+    for (let i = 0; i < ListOfLanguages.length; i++) {
+        if (ListOfLanguages[i][1] === lang) {
+            return ListOfLanguages[i][0];
+        }
+    }
+    return ""; // Handle case where language code is not found
+}
 
-export default translate
+export default translate;
